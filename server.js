699fc11694211b0d5da34f5cf3186acf9a104c35
hakanyaman <hakanyaman5249@gmail.com>
2023-04-20 04:23:21 +0300

    Promise reddedildi

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+process.on('rejectionHandled',err=>{
+  server.close(()=>{
+    console.log(`UNCAUGHT EXCEPTION! 💥 Shutting down...`);
+    process.exit(1);
+  })
+})
+
 const app = require('./app');
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose
-  .connect(DB, {
+
+mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
